4770090c7cdaad4e2f51224294f4b210c97e80c2
Gabriela Tenório <gabitenoriob@gmail.com>
2023-08-19 16:05:58 -0300

    Update consulta-nomes.component.ts

--- src/app/components/consulta/consulta-nomes/consulta-nomes.component.ts
+++ src/app/components/consulta/consulta-nomes/consulta-nomes.component.ts
@@ -2,13 +2,16 @@ import { AfterViewInit, Component } from '@angular/core';
 import { IbgeApiService } from '../../../ibge-api.service';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-consulta-nomes',
   templateUrl: './consulta-nomes.component.html',
   styleUrls: ['./consulta-nomes.component.css']
 })
-export class ConsultaNomesComponent implements AfterViewInit{
+export class ConsultaNomesComponent implements AfterViewInit {
   form!: FormGroup;
   loader = false;
   name: string = '';
@@ -16,26 +19,43 @@ export class ConsultaNomesComponent implements AfterViewInit{
   displayedColumns: string[] = ['periodo', 'frequencia'];
   dataSource = new MatTableDataSource<any>([]);
 
-ngAfterViewInit(): void {
-  this.dataSource.data = this.nameData[0].res
-}
-  constructor(private ibgeApiService: IbgeApiService) {
+  constructor(
+    private ibgeApiService: IbgeApiService,
+    private router: Router
+  ) {
     this.form = new FormGroup({
-      name: new FormControl('', Validators.required)
-    })
+      name: new FormControl('', [Validators.required, Validators.pattern('[a-zA-Z ]*')])
+    });
+    
   }
 
-  submitForm() { // depois que o usuario envia o formulario verifica se há um nome
+  ngAfterViewInit(): void {
+    this.dataSource.data = this.nameData[0].res;
+  }
+
+  submitForm() {
     this.loader = true;
     this.name = this.form.get('name')?.value;
     if (this.name) {
-      this.ibgeApiService.getNameData(this.name) //atribui os resultados em nameData
+      this.ibgeApiService.getNameData(this.name)
         .subscribe(data => {
           this.nameData = data;
-          this.dataSource.data = data[0].res
+          this.dataSource.data = data[0].res;
           this.loader = false;
-
         });
     }
+   
+  }
+
+  getTotalFrequencia(): number {
+    if (this.nameData) {
+      return this.nameData[0].res.reduce((total: number, element: any) => total + element.frequencia, 0);
+    }
+    return 0;
+  }
+  
+
+  cancelar() {
+    this.router.navigate(['/']);
   }
 }
